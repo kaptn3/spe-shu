40d107052eccdb769dca75915b68e218d0723c87
victoria <kapitonenko.vick@ya.ru>
2019-08-02 20:24:46 +0300

    feat: add error to form

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -2,12 +2,45 @@ var form = new Vue({
   el: '.form',
   data() {
     return {
-      type: 'password'
+      type: 'password',
+      login: '',
+      password: '',
+      error: ['Логин не зарегистрирован', 'Проверьте данные']
     };
+  },
+  computed: {
+    loginClass() {
+      return {
+        'form__input_fill': this.login.length > 6,
+        'form__input_error': this.error[0]
+      };
+    },
+    passwordClass() {
+      return {
+        'form__input_fill': this.password.length > 6,
+        'form__input_error': this.error[1]
+      };
+    },
   },
   methods: {
     toggleShow() {
       this.type = this.type === 'text' ? 'password' : 'text';
+    }
+  },
+  watch: {
+    login() {
+      if (this.login.length > 0) {
+        this.error[0] = '';
+      } else {
+        this.error[0] = 'Логин не зарегистрирован';
+      }
+    },
+    password() {
+      if (this.password.length > 0) {
+        this.error[1] = '';
+      } else {
+        this.error[1] = 'Проверьте данные';
+      }
     }
   }
 });
