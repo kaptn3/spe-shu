6d5903aaa8a4e77c46cba3e51531c63d88a68d67
victoria <kapitonenko.vick@ya.ru>
2019-08-04 00:31:11 +0300

    feat: appearance download btn

--- src/assets/js/table.js
+++ src/assets/js/table.js
@@ -1,11 +1,27 @@
 const checkInput = document.querySelector('input[name="docs"]');
+const checkboxes = document.querySelectorAll('input[name="doc"]');
 
-if (checkInput) {
+const showButton = () => {
+  let checked = 0;
+  for (let k = 0; k < checkboxes.length; k++) {
+    if (checkboxes[k].checked) checked++;
+  }
+  const height = checked === 0 ? '0' : '56px';
+  document.querySelector('.download-btn').style.height = height;
+}
+
+if (checkInput && checkboxes) {
   checkInput.addEventListener('change', () => {
-    const checkboxes = document.querySelectorAll('input[name="doc"]');
-
     for (let i = 0; i < checkboxes.length; i++) {
       checkboxes[i].checked = checkInput.checked;
     }
+    showButton();
   });
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('change', () => {
+      showButton();
+    });
+  }
 }
+
