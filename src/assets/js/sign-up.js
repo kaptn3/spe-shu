29aeebf829b201ac094d68a634df7538edb8abce
victoria <kapitonenko.vick@ya.ru>
2019-08-07 21:42:23 +0300

    feat: disabled btn, timer for repeat code

--- src/assets/js/sign-up.js
+++ src/assets/js/sign-up.js
@@ -2,19 +2,34 @@ const signUp = new Vue({
   el: '#sign-up',
   data() {
     return {
-      error: false
+      error: false,
+      sec: 45,
+      interval: null,
+      phoneLength: 0
     };
   },
   methods: {
     checkForm(e) {
       e.preventDefault();
       this.error = true;
+    },
+    getCode() {
+      this.interval = setInterval(() => {
+        if (this.sec > 0) {
+          this.sec -= 1;
+        } else {
+          clearInterval(this.interval);
+        }
+      }, 1000);
+    },
+    inputPhone() {
+      this.phoneLength = phone.value.length;
     }
   }
 });
 
-var element = document.querySelector("input[type='tel']");
+var phone = document.querySelector("input[type='tel']");
 var maskOptions = {
   mask: '+{7} (000) 000-00-00'
 };
-var mask = IMask(element, maskOptions);
+var mask = IMask(phone, maskOptions);
