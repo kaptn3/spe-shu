f09d1990bb59ec6fbb8e6e5830c4523eec09f77b
victoria <kapitonenko.vick@ya.ru>
2019-08-08 09:46:31 +0300

    feat: add popup for profile

---
+++ src/assets/js/header-profile.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('.header__profile');
+const popup = btn.querySelector('.popup');
+let isOpen = false;
+
+btn.addEventListener('click', () => {
+  if (isOpen) {
+    popup.style.display = 'none';
+  } else {
+    popup.style.display = 'block';
+  }
+
+  isOpen = !isOpen;
+});
